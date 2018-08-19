553e42c2785e6032c94762f85c170f20a7897dac
ahmed atta <ahmedatta3322@gmail.com>
2018-08-19 17:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+function getFirstSelector(s) {
+  return document.querySelector(s);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+function deepestChild(){
+  var x = document.getElementById('grand-node').querySelectorAll('div')
+  return x[x.length-1]
+}
+function increaseRankBy(n){
+  let x = document.getElementsByClassName('ranked-list')
+  let y = []
+  let z = []
+  for (var i = 0 ; i < x.length ; i++){
+    y = x[i]
+    for(var c = 0 ; c < y.length ; c++){
+      z = y.pop()
+    }
+  }
+  return z
+}
